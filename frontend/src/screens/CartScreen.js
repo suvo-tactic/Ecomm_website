06d8faa1d075f1c48f88a0c297844d27b86da097
Web-dev009 <shubhopal111@gmail.com>
2022-07-24 16:09:55 +0530

    Initial Commit

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,26 @@
+import './CartScreen.css'
+import CartItem from '../components/CartItem';
+const CartScreen = () => {
+    return (
+        <div className="cartscreen">
+        <div className="cartscreen_left">
+            <h2>Shopping Cart</h2>
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+        </div>
+        <div className="cartscreen_right">
+            <div className="cartscreen_info">
+                <p>Subtotal (0)</p>
+                <p>$499.99</p>
+            </div>
+            <div>
+                <button>Proceed to Checkout</button>
+            </div>
+        </div>
+        </div>
+    )
+};
+
+export default CartScreen;
